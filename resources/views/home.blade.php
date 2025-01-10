062b00c3ddf3e1f7f60aceb267f7526a01e0fe5b
Kevin Manceau <kevin.manceau@gmail.com>
2025-01-10 17:02:42 +0100

    removed the navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,23 +11,6 @@
 </head>
 
 <body>
-<nav class="bg-gray-800 p-4">
-    <div class="container mx-auto flex justify-between items-center">
-        <a href="#" class="text-white text-xl font-bold">ChillFlix</a>
-        <div class="hidden md:flex space-x-4">
-            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
-            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
-            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Services</a>
-            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</a>
-        </div>
-        <div class="md:hidden">
-            <button class="text-gray-300 hover:text-white focus:outline-none focus:text-white">
-                <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-                </svg>
-            </button>
-        </div>
-    </div>
-</nav>
+
 </body>
 </html>
