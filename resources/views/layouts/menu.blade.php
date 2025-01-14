e4289dae3033113f595025c451b715e8200a22d5
Kevin Manceau <kevin.manceau@gmail.com>
2025-01-14 10:40:00 +0100

    Improvec main menu html

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,36 +1,34 @@
-<section id="main-menu-container">
-    <nav >
-        <ul class="main-menu">
-            <li class="menu-item">
-                <a href="/" class="menu-link">
-                    <i class="bi bi-house-fill"></i><span class="not-on-mobile">Home</span>
-                </a>
-            </li>
-            <li class="menu-item">
-                <a href="/" class="menu-link">
-                    <i class="bi bi-film"></i><span class="not-on-mobile">Movies</span>
-                </a>
-            </li>
-            <li class="menu-item">
-                <a href="/" class="menu-link">
-                    <i class="bi bi-tv"></i><span class="not-on-mobile">TV Shows</span>
-                </a>
-            </li>
-            <li class="menu-item">
-                <a href="/" class="menu-link">
-                    <i class="bi bi-star-fill"></i><span class="not-on-mobile">Favoris</span>
-                </a>
-            </li>
-            <li class="menu-item">
-                <a href="/" class="menu-link">
-                    <i class="bi bi-stopwatch-fill"></i><span class="not-on-mobile">Watchlist</span>
-                </a>
-            </li>
-            <li class="menu-item">
-                <a href="/" class="menu-link">
-                    <i class="bi bi-person-fill"></i><span class="not-on-mobile">Account</span>
-                </a>
-            </li>
-        </ul>
-    </nav>
-</section>
+<nav id="main-menu-container">
+    <ul class="main-menu">
+        <li class="menu-item">
+            <a href="/" class="menu-link">
+                <i class="bi bi-house-fill"></i><span class="not-on-mobile">Home</span>
+            </a>
+        </li>
+        <li class="menu-item">
+            <a href="/" class="menu-link">
+                <i class="bi bi-film"></i><span class="not-on-mobile">Movies</span>
+            </a>
+        </li>
+        <li class="menu-item">
+            <a href="/" class="menu-link">
+                <i class="bi bi-tv"></i><span class="not-on-mobile">TV Shows</span>
+            </a>
+        </li>
+        <li class="menu-item">
+            <a href="/" class="menu-link">
+                <i class="bi bi-star-fill"></i><span class="not-on-mobile">Favoris</span>
+            </a>
+        </li>
+        <li class="menu-item">
+            <a href="/" class="menu-link">
+                <i class="bi bi-stopwatch-fill"></i><span class="not-on-mobile">Watchlist</span>
+            </a>
+        </li>
+        <li class="menu-item">
+            <a href="/" class="menu-link">
+                <i class="bi bi-person-fill"></i><span class="not-on-mobile">Account</span>
+            </a>
+        </li>
+    </ul>
+</nav>
