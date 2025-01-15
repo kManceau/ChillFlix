97a52dc4776de3bae605774dabcfc7c156768722
Kevin Manceau <kevin.manceau@gmail.com>
2025-01-15 11:16:45 +0100

    Basic movie page ok.

--- resources/views/item.blade.php
+++ resources/views/item.blade.php
@@ -6,6 +6,16 @@
         <img src="https://image.tmdb.org/t/p/w500{{ $item['poster_path'] }}" alt="Affiche de {{ $item['title'] }}" class="item-poster">
         <div class="item-infos">
             <h2>{{$item['title']}}</h2>
+            <div class="item-genre">
+                @foreach ($item['genres'] as $genre)
+                    <p class="genre">{{ $genre['name'] }}</p>
+                @endforeach
+            </div>
+            <div class="item-icons">
+                <a href="#" title="Ajouter aux favoris"><i class="bi bi-star"></i></a>
+                <a href="#" title="Ajouter à la watchlist"><i class="bi bi-stopwatch"></i></a>
+            </div>
+            <p class="item-overview">{{ $item['overview'] }}</p>
         </div>
     </section>
 @endsection
